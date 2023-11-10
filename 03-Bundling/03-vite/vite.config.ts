2bfc96faa4ccc67086828e46a6091598b93c9b18
Raúl Núñez <r4nu54@gmail.com>
2023-11-10 22:24:40 +0100

    Add Vite practices 03-bundling module.

---
+++ 03-Bundling/03-vite/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+
+import checker from 'vite-plugin-checker'
+
+import typescript from '@rollup/plugin-typescript'
+
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [checker({ typescript: true }), react()],
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      plugins: [typescript({ tsconfig: 'tsconfig.json' })]
+    }
+  }
+})
