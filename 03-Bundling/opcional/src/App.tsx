9888ded9899e8a644a1587a56f63fb7ce069758b
Raúl Núñez <r4nu54@gmail.com>
2023-11-22 00:23:13 +0100

    refactor: refector optional exercise module 03 bundling

---
+++ 03-Bundling/opcional/src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function App() {
+  return <h1>Hello from React DOM</h1>
+}
+
+console.log(`Api URL: ${process.env.API_BASE}`)
+
+export default App
---
+++ 03-Bundling/opcional/webpack.dev.js
@@ -0,0 +1,50 @@
+import { merge } from 'webpack-merge'
+import Dotenv from 'dotenv-webpack'
+
+import common from './webpack.common.js'
+import path from 'node:path'
+import url from 'node:url'
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url))
+
+export default merge(common, {
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                exportLocalsConvention: 'camelCase',
+                localIdentName: '[path][name]__[local]--[hash:base64:5]'
+              }
+            }
+          },
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new Dotenv({
+      path: './dev.env'
+    })
+  ],
+  devtool: 'eval-source-map',
+  devServer: {
+    port: 8080,
+    open: true,
+    hot: true,
+    static: {
+      directory: path.join(__dirname, 'src')
+    },
+    devMiddleware: {
+      stats: 'errors-only'
+    }
+  }
+})
