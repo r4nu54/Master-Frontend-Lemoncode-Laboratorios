1380d33b0cccb2d4be4bea2eb527f556fd66896a
Raúl Núñez <r4nu54@gmail.com>
2023-12-19 21:41:01 +0100

    refactor: refactor fetch and components tsx.

--- 04-Frameworks/basico/src/app.tsx
+++ 04-Frameworks/basico/src/app.tsx
@@ -1,17 +1,17 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { LoginPage } from "./login";
-import { ListPage } from "./list";
-import { DetailPage } from "./detail";
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { LoginPage } from '@/login'
+import { ListPage } from '@/list'
+import { DetailPage } from '@/detail'
 
-export const App = () => {
+export const App = (): JSX.Element => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/list" element={<ListPage />} />
-        <Route path="/detail/:id" element={<DetailPage />} />
+        <Route path='/' element={<LoginPage />} />
+        <Route path='/list' element={<ListPage />} />
+        <Route path='/detail/:id' element={<DetailPage />} />
       </Routes>
     </Router>
-  );
-};
+  )
+}
--- 04-Frameworks/basico/src/login.tsx
+++ 04-Frameworks/basico/src/login.tsx
@@ -1,46 +1,37 @@
-import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export const LoginPage: React.FC = () => {
-  const navigate = useNavigate();
-  const [username, setUsername] = React.useState("");
-  const [password, setPassword] = React.useState("");
+  const navigate = useNavigate()
+  const [username, setUsername] = React.useState('')
+  const [password, setPassword] = React.useState('')
 
-  const handleNavigation = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const handleNavigation = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault()
 
-    if (username === "admin" && password === "test") {
-      navigate("/list");
+    if (username === '' && password === '') {
+      navigate('/list')
     } else {
-      alert("User / password not valid, psst... admin / test");
+      alert('User / password not valid, psst... admin / test')
     }
-  };
+  }
 
   return (
     <>
       <form onSubmit={handleNavigation}>
         <h2>Hello from login page</h2>
-
         <div>
           <div>
             <label>Username: </label>
-            <input
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
+            <input value={username} onChange={e => setUsername(e.target.value)} />
           </div>
           <div>
             <label>Password: </label>
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
+            <input type='password' value={password} onChange={e => setPassword(e.target.value)} />
           </div>
         </div>
-
-        <button type="submit">Login</button>
+        <button type='submit'>Login</button>
       </form>
     </>
-  );
-};
+  )
+}
