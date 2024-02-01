5eafca9d0cfe3c7fc4d50d41fb5d44a92b4f71c3
Raúl Núñez <raul.nunez.santiago@outlook.es>
2024-02-01 12:16:48 +0100

    Delete unused API and scene files

--- 04-Frameworks/React/src/core/api/member.api.ts
+++
@@ -1,8 +0,0 @@
-export const getMembersByOrg = async (org: string) => {
-  try {
-    // await fetch('https://657b7dec394ca9e4af1467db.mockapi.io/members');
-    await fetch(`https://api.github.com/orgs/${org}/members`).then((response) => response.json());
-  } catch (error) {
-    console.log(error);
-  }
-};
