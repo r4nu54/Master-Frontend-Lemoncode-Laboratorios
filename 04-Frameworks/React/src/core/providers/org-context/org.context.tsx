b8ba8abeafa4c2230ed81acf0764219a3f30acaa
Raúl Núñez <raul.nunez.santiago@outlook.es>
2024-02-26 00:33:53 +0100

    Update OrgContext to include isError and setIsError

--- 04-Frameworks/React/src/core/providers/org-context/org.context.tsx
+++ 04-Frameworks/React/src/core/providers/org-context/org.context.tsx
@@ -6,10 +6,16 @@ interface Props {
   children: React.ReactNode;
 }
 
-export const OrgContext = createContext<OrgModel>({ orgName: '', setOrgName: () => {} });
+export const OrgContext = createContext<OrgModel>({
+  orgName: '',
+  setOrgName: () => {},
+  isError: false,
+  setIsError: () => {},
+});
 
 export const OrgProvider: React.FC<Props> = ({ children }) => {
   const [orgName, setOrgName] = useState<string>('Lemoncode');
+  const [isError, setIsError] = useState<boolean>(false);
 
-  return <OrgContext.Provider value={{ orgName: orgName, setOrgName }}>{children}</OrgContext.Provider>;
+  return <OrgContext.Provider value={{ orgName, setOrgName, isError, setIsError }}>{children}</OrgContext.Provider>;
 };
--- 04-Frameworks/React/src/core/providers/org-context/org.context.vm.ts
+++ 04-Frameworks/React/src/core/providers/org-context/org.context.vm.ts
@@ -1,4 +1,6 @@
 export interface OrgModel {
   orgName: string;
   setOrgName: (orgName: string) => void;
+  isError: boolean;
+  setIsError: (isError: boolean) => void;
 }
