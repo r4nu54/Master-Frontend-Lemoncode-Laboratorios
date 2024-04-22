0b07f3defbcdb5760478e011adf2d4f233ffdba3
Raúl Núñez <raul.nunez.santiago@outlook.es>
2024-04-22 21:21:14 +0200

    Update API call in getMembersByOrg function

--- 04-Frameworks/React/src/pods/github-list/api/api.ts
+++ 04-Frameworks/React/src/pods/github-list/api/api.ts
@@ -4,10 +4,7 @@ import { MemberEntityApi, ResponseApi } from './index';
 export const getMembersByOrg = async (org: string, perPage: number, currentPage: number): Promise<ResponseApi> => {
   const url = `https://api.github.com/orgs/${org}/members?page=${currentPage}&per_page=${perPage}`;
 
-  const token = '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';
-
-  // Eliminar el token y el header cuando termine el Laboratorio.
-  const response = await axios.get(url, { headers: { Authorization: `Bearer ${token}` } });
+  const response = await axios.get(url);
 
   const linkHeader = response.headers['link'];
   const data: MemberEntityApi[] = response.data;
